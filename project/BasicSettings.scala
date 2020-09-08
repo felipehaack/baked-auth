769a3d26873eec43c3d10c737f0651d3f62dceb4
FelipeHS <felipe.haackschmitz@gmail.com>
2020-09-09 00:36:19 +0200

    new dependencies and improvements over the basic settings

--- project/BasicSettings.scala
+++ project/BasicSettings.scala
@@ -3,28 +3,37 @@ import sbt._
 
 object BasicSettings extends AutoPlugin {
 
-  lazy val Tools         = config("tools") extend Compile
-  lazy val toolsSettings = inConfig(Tools)(Defaults.compileSettings)
-
   override lazy val trigger = allRequirements
 
-  lazy val settings: Seq[Def.Setting[_]] = Seq(
-    name := "market-api",
-    scalaVersion := "2.12.10",
-    organization := "com.market.app",
-    scalacOptions ++= Seq(
-          "-deprecation",     // Emit warning and location for usages of deprecated APIs.
-          "-feature",         // Emit warning and location for usages of features that should be imported explicitly.
-          "-unchecked",       // Enable additional warnings where generated code depends on assumptions.
-          "-Xfatal-warnings", // Fail the compilation if there are any warnings.
-          "-encoding",
-          "UTF-8",                         // Adding default encode type
-          "-language:implicitConversions", // Otherwise the compiler will ask to import scala.language.implicitConversions
-          "-language:higherKinds",         // Enable by default the high kinds for cats
-          "-language:postfixOps",          // Enable by default post fix ops for cats
-          "-Ypartial-unification"          // This is optional, but it is better to keep as it might result in fail on Http4s
-        )
-  )
+  lazy val settings: Seq[Def.Setting[_]] =
+    Seq(
+      scalaVersion := "2.12.12",
+      organization := "com.pays.market",
+      resolvers ++= Seq(
+            Resolver.jcenterRepo,
+            Resolver.mavenCentral,
+            "repo1" at "https://repo1.maven.org/maven2/"
+          ),
+      scalacOptions ++= Seq(
+            "-deprecation",     // Emit warning and location for usages of deprecated APIs.
+            "-feature",         // Emit warning and location for usages of features that should be imported explicitly.
+            "-unchecked",       // Enable additional warnings where generated code depends on assumptions.
+            "-Xfatal-warnings", // Fail the compilation if there are any warnings.
+            "-encoding",
+            "UTF-8",                         // Adding default encode type
+            "-language:implicitConversions", // Otherwise the compiler will ask to import scala.language.implicitConversions
+            "-language:higherKinds",         // Enable by default the high kinds for cats
+            "-language:postfixOps",          // Enable by default post fix ops for cats
+            "-Ypartial-unification"          // This is optional, but it is better to keep as it might result in fail on Http4s
+          ),
+      javacOptions ++= Seq(
+            "-source",
+            "1.8",
+            "-target",
+            "1.8",
+            "-Xlint"
+          )
+    )
 
   val itSettings = Defaults.itSettings ++ Seq(
           fork in IntegrationTest := true,
@@ -34,8 +43,8 @@ object BasicSettings extends AutoPlugin {
         )
 
   override def projectSettings: Seq[Setting[_]] =
-    super.projectSettings ++ settings ++ toolsSettings ++ itSettings
+    super.projectSettings ++ settings ++ itSettings
 
   override def projectConfigurations =
-    super.projectConfigurations :+ Tools :+ IntegrationTest
+    super.projectConfigurations :+ IntegrationTest
 }
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,14 +4,10 @@ object Dependencies extends AutoPlugin {
 
   val autoImport = this
 
-  object Cassandra {
-    val driver = "com.datastax.oss" % "java-driver-core" % "4.8.0"
-  }
-
   object Cats {
     val version = "2.1.1"
-    val core    = "org.typelevel" %% "cats-core"   % version
     val effect  = "org.typelevel" %% "cats-effect" % version
+    val core    = "org.typelevel" %% "cats-core"   % version
   }
 
   object Circe {
@@ -21,10 +17,29 @@ object Dependencies extends AutoPlugin {
     val parser          = "io.circe" %% "circe-parser"  % version
   }
 
+  object DB {
+    val hikaricp  = "com.zaxxer"       % "HikariCP"       % "3.4.5"
+    val liquibase = "org.liquibase"    % "liquibase-core" % "4.0.0"
+    val postgres  = "org.postgresql"   % "postgresql"     % "42.2.16"
+    val scalike   = "org.scalikejdbc" %% "scalikejdbc"    % "3.5.0"
+  }
+
   object Http4s {
     private val version = "0.21.7"
-    val server          = "org.http4s" %% "http4s-blaze-server" % version
     val dsl             = "org.http4s" %% "http4s-dsl"          % version
+    val server          = "org.http4s" %% "http4s-blaze-server" % version
+  }
+
+  object Macro {
+    private val paradiseVersion = "2.1.1"
+    private val reflectVersion  = "2.12.9"
+    val paradise                = "org.scalamacros" % "paradise"      % paradiseVersion cross CrossVersion.full
+    val reflect                 = "org.scala-lang"  % "scala-reflect" % reflectVersion
+  }
+
+  object Secure {
+    val bcrypt = "com.github.t3hnar" %% "scala-bcrypt" % "4.1"
+    val jwt    = "com.auth0"          % "java-jwt"     % "3.10.3"
   }
 
   object Specs2 {
@@ -36,8 +51,8 @@ object Dependencies extends AutoPlugin {
   }
 
   object Utils {
+    val accord  = "com.wix"       %% "accord-core"     % "0.7.6"
     val ficus   = "com.iheart"    %% "ficus"           % "1.5.0"
     val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
   }
-
 }
