2e385f354e568125236fe3122bdd7cd89a871b0a
FelipeHS <felipe.haackschmitz@gmail.com>
2020-11-20 15:53:42 +0100

    enhanced login service to provide social login flow based on user me model

--- market-api/src/main/scala/com/pays/market/api/service/login/LoginService.scala
+++ market-api/src/main/scala/com/pays/market/api/service/login/LoginService.scala
@@ -6,15 +6,17 @@ import cats.effect.Sync
 import cats.syntax.ApplicativeErrorSyntax
 import com.github.t3hnar.bcrypt._
 import com.pays.market.api.db.PostgresDb
-import com.pays.market.api.model.MarketApiException
+import com.pays.market.api.model.{ MarketApiException, MarketNotFoundException }
 import com.pays.market.api.service.password.UserPasswordService
-import com.pays.market.api.service.user.UserService
+import com.pays.market.api.service.social.model.Login.Me
+import com.pays.market.api.service.user.{ User, UserService }
 import com.pays.market.api.util.{ JwtCodec, Log }
 
 import scala.util.{ Failure, Success }
 
 trait LoginService[F[_]] extends ApplicativeErrorSyntax with Log {
-  def create(login: Login): F[Login.Token]
+  def createNormal(login: Login): F[Login.Token]
+  def createSocial(me: Me): F[Login.Token]
 }
 
 object LoginService {
@@ -28,7 +30,7 @@ object LoginService {
     implicit A: ApplicativeError[F, Throwable]
   ): LoginService[F] =
     new LoginService[F] {
-      override def create(
+      override def createNormal(
         login: Login
       ): F[Login.Token] = {
         val r = for {
@@ -44,10 +46,40 @@ object LoginService {
           token <- jwtCodec.encode(user)
         } yield Login.Token.apply(token)
 
-        r.handleErrorWith { e =>
-          logger.info("login failed with following error", e)
-          A.raiseError[Login.Token](MarketApiException.invalid("user_or_password"))
+        r.handleErrorWith(handleLoginError)
+      }
+
+      override def createSocial(me: Me): F[Login.Token] = {
+        val r = for {
+          user  <- getOrCreate(me)
+          token <- jwtCodec.encode(user)
+        } yield Login.Token.apply(token)
+
+        r.handleErrorWith(handleLoginError)
+      }
+
+      private def getOrCreate(me: Me): F[User] = {
+        def create(me: Me): F[User] = {
+          val createUser = User.Create(
+            name = me.name,
+            email = me.email
+          )
+          for {
+            userId <- userService.create(createUser)
+            user   <- userService.getById(userId)
+          } yield user
         }
+        userService
+          .getByEmail(me.email)
+          .handleErrorWith {
+            case _: MarketNotFoundException => create(me)
+            case e                          => A.raiseError(e)
+          }
+      }
+
+      private def handleLoginError(t: Throwable): F[Login.Token] = {
+        logger.info("login failed with following error", t)
+        A.raiseError[Login.Token](MarketApiException.invalid("user_or_password"))
       }
     }
 }
