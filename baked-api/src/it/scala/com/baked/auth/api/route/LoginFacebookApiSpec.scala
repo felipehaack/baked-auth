83ba833746839e0ac40163267526f3fc5d79cade
FelipeHS <felipe.haackschmitz@gmail.com>
2021-02-24 22:17:32 +0100

    created integration tests

---
+++ baked-api/src/it/scala/com/baked/auth/api/route/LoginFacebookApiSpec.scala
@@ -0,0 +1,49 @@
+package com.baked.auth.api.route
+
+import cats.effect.IO
+import com.baked.auth.api.ItSpec
+import com.baked.auth.api.service.login.Login.Token
+import com.baked.auth.api.service.social.model.Login.WithAccessToken
+import org.http4s.Status
+import org.specs2.matcher.MatchResult
+
+class LoginFacebookApiSpec extends ItSpec {
+
+  private val path = "/api/login/facebook"
+
+  "LoginFacebookApi" >> {
+    def assertOK(login: WithAccessToken): IO[MatchResult[Either[Throwable, Unit]]] =
+      apis.use { implicit routes =>
+        for {
+          response <- post"$path".withJsonObj(login).compile
+          _ = response.status must beEqualTo(Status.Ok)
+          t       <- response.bodyAsJson[Token]
+          isValid <- jwtCodec.isValid(t.token).attempt
+        } yield isValid must beRight
+      }
+    "return OK for an valid and existing user" in {
+      val login = WithAccessToken.apply(
+        token = "email@email.com"
+      )
+      assertOK(login)
+    }
+    "return OK for a new created user" in {
+      val loginNewUser = WithAccessToken.apply(
+        token = "newuser@email.com"
+      )
+      assertOK(loginNewUser)
+    }
+    "json validation" >> {
+      def checkBadRequest(login: WithAccessToken): IO[MatchResult[Status]] =
+        apis.use { implicit routes =>
+          for {
+            response <- post"$path".withJsonObj(login).compile
+          } yield response.status must beEqualTo(Status.BadRequest)
+        }
+      "return BAD_REQUEST for an empty token" in {
+        val emptyLogin = WithAccessToken.apply("")
+        checkBadRequest(emptyLogin)
+      }
+    }
+  }
+}
